831350d2801ef59e24964fd7423621800879ed44
Zack (Hang Yee) Wong <Zack.wong@NYCL-DADAMS.take2.t2.corp>
2021-06-16 14:02:40 -0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# syntax=docker/dockerfile:1
+
+FROM python:3.8-slim-buster
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+
+COPY . .
+
+CMD ["set"]
+
+#CMD [ "python3", "app.py" ]
+
+CMD tail -f /dev/null
---
+++ app.py
@@ -0,0 +1,35 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import os
+import sys
+
+##################################################
+#var
+##################################################
+token = sys.argv[1]
+owner = sys.argv[2]
+repo = sys.argv[3]
+url_get_release_latest_tag = "https://api.github.com/repos/" + owner + "/"+ repo + "/releases/latest"
+url_download_release_latest = "https://github.com/" + owner + "/"+ repo + "/archive/"
+
+##################################################
+#helper functions
+##################################################
+def download_url( url , save_path, chunk_size=128 ):
+    r = requests.get( url, stream = True, headers={ 'Authorization' : 'token '+ token })
+    with open( save_path , 'wb' ) as fd:
+        for chunk in r.iter_content( chunk_size = chunk_size ):
+            fd.write( chunk )
+
+##################################################
+#Script start here
+##################################################
+print(url_get_release_latest_tag)
+print(url_download_release_latest)
+data = requests.get( url_get_release_latest_tag , headers = { 'Authorization' : 'token ' + token })
+dataObj = json.loads( data.content )
+print(dataObj)
+latestTag = dataObj[ 'tag_name' ]
+download_url( url_download_release_latest + '/' + latestTag + '.zip', os.getcwd()+'/' +latestTag + '.zip')
+print ('Argument List: '+ str(sys.argv))
