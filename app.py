54f5a66a86199cf7ea9b3a0e193803a80724bae5
Zack (Hang Yee) Wong <Zack.wong@NYCL-DADAMS.take2.t2.corp>
2021-06-17 09:57:17 -0700

    update

--- app.py
+++ app.py
@@ -44,6 +44,6 @@ download_url( src_download_file_path , dst_download_file_path )
 # check file exists
 if os.path.exists(dst_download_file_path):
     print( "File exists: " + dst_download_file_path )
-    print( "File size (bytes): " + os.path.getsize(dst_download_file_path))
+    print( "File size (bytes): " + str(os.path.getsize(dst_download_file_path)))
 else:
     print( "File dose not exist: " + dst_download_file_path )
