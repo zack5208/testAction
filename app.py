d4c8af838333132344d33d57bc56fdf3db9ed519
Zack (Hang Yee) Wong <Zack.wong@NYCL-DADAMS.take2.t2.corp>
2021-06-21 15:57:20 -0700

    updated

--- app.py
+++ app.py
@@ -46,7 +46,10 @@ def upload_file(file_name, bucket,ACCESS_KEY ,SECRET_KEY,SESSION_TOKEN, object_n
 
     # Upload the file
     s3_client = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY)
+    aws_secret_access_key=SECRET_KEY,
+    aws_session_token=SESSION_TOKEN)
+    #s3_client = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+    #aws_secret_access_key=SECRET_KEY)
     try:
         response = s3_client.upload_file(file_name, bucket, object_name)
     except ClientError as e:
@@ -93,8 +96,8 @@ if os.path.exists(dst_download_file_path):
     print( "File size (bytes): " + str(os.path.getsize(dst_download_file_path)))
     # Upload zip file to S3
     repo_name_arr = repo.split("/")
-    file_object_name = repo_name_arr[0] + "-" + repo_name_arr[1] + "-" + download_file_name
-    #file_object_name = repo_name_arr[1] + "/" + download_file_name  
+    #file_object_name = repo_name_arr[0] + "-" + repo_name_arr[1] + "-" + download_file_name
+    file_object_name = repo_name_arr[1] + "/" + download_file_name  
     if upload_file(dst_download_file_path,s3_bucket,ACCESS_KEY,SECRET_KEY,SESSION_TOKEN,file_object_name):
         print ("Upload Successful!")
     else:
