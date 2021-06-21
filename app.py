157e4f00aadba0f592b96e04eeec98d541b7e8d3
Zack (Hang Yee) Wong <Zack.wong@NYCL-DADAMS.take2.t2.corp>
2021-06-21 16:30:21 -0700

    updated

--- app.py
+++ app.py
@@ -26,7 +26,8 @@ print(os.environ)
 #helper functions
 ##################################################
 def download_url( url , save_path, chunk_size=128 ):
-    r = requests.get( url, stream = True, headers={ 'Authorization' : 'token '+ token })
+    #r = requests.get( url, stream = True, headers={ 'Authorization' : 'token '+ token })
+    r = requests.get( url, stream = True)
     with open( save_path , 'wb' ) as fd:
         for chunk in r.iter_content( chunk_size = chunk_size ):
             fd.write( chunk )
